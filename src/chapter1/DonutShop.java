4a5324e7e0d2a01e63144c7626c15bd264183037
sklee <hadesleeq@gmail.com>
2018-11-22 11:02:03 +0900

    The Tuple class

--- src/chapter1/DonutShop.java
+++ src/chapter1/DonutShop.java
@@ -3,7 +3,7 @@ package chapter1;
 public class DonutShop {
     public static Tuple<Donut, Payment> buyDount(CreditCard creditCard) {
         Donut dount = new Donut();
-        creditCard.charge(Donut.price);
-        return dount;
+        Payment payment = new Payment(creditCard, (Integer) Donut.price);
+        return new Tuple<>(dount, payment);
     }
 }
