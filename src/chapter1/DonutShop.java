6fa8d2629dee9305326ae812035591c9da2bea20
sklee <hadesleeq@gmail.com>
2018-11-26 10:38:13 +0900

    Buying multiple donuts at once

--- src/chapter1/DonutShop.java
+++ src/chapter1/DonutShop.java
@@ -3,11 +3,12 @@ package chapter1;
 import java.util.List;
 
 public class DonutShop {
-    public static Tuple<Donut, Payment> buyDount(CreditCard creditCard) {
+    public static Tuple<Donut, Payment> buyDonut(final CreditCard cCard) {
         return new Tuple<>(new Donut(), new Payment(cCard, Donut.price));
     }
-
-    public static Tuple<List<Donut>, Payment> buyDonuts(final int quantity, final CreditCard creditCard) {
-        return new Tuple<fill(quantity, ()->new Donut()),new Payment(cCard, Donut.price * quantity);
+    public static Tuple<List<Donut>, Payment> buyDonuts(final int quantity,
+                                                        final CreditCard cCard) {
+        return new Tuple<>(fill(quantity, () -> new Donut()),
+                new Payment(cCard, Donut.price * quantity));
     }
 }
