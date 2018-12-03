0ec3e06fb821ee4d59ef118ea313f6c7a2c65651
sklee <hadesleeq@gmail.com>
2018-12-03 09:12:55 +0900

    The email validation application with no control structures

--- src/chapter3/EmailValidation.java
+++ src/chapter3/EmailValidation.java
@@ -4,6 +4,11 @@ import chapter2.Function;
 
 import java.util.regex.Pattern;
 
+import static chapter3.Case.match;
+import static chapter3.Case.mcase;
+import static chapter3.Result.failure;
+import static chapter3.Result.success;
+
 public class EmailValidation {
     static Pattern emailPattern =
             Pattern.compile("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
@@ -11,12 +16,14 @@ public class EmailValidation {
             System.out.println("Mail sent to " + s);
     static Effect<String> failure = s ->
             System.err.println("Error message logged: " + s);
+
     public static void main(String... args) {
         emailChecker.apply("this.is@my.email").bind(success, failure);
         emailChecker.apply(null).bind(success, failure);
         emailChecker.apply("").bind(success, failure);
         emailChecker.apply("john.doe@acme.com").bind(success, failure);
     }
+
     static Function<String, Result<String>> emailChecker = s -> match(
             mcase(() -> success(s)),
             mcase(() -> s == null, () -> failure("email must not be null")),
