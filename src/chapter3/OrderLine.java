1370e9d676aae0f420e29befe11caaa9d3303a31
sklee <hadesleeq@gmail.com>
2018-12-06 13:14:10 +0900

    The component representing one line of an order

---
+++ src/chapter3/OrderLine.java
@@ -0,0 +1,29 @@
+package chapter3;
+
+public class OrderLine {
+    private Product product;
+    private int count;
+    public OrderLine(Product product, int count) {
+        super();
+        this.product = product;
+        this.count = count;
+    }
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    public int getCount() {
+        return count;
+    }
+    public void setCount(int count) {
+        this.count = count;
+    }
+    public double getWeight() {
+        return this.product.getWeight() * this.count;
+    }
+    public double getAmount() {
+        return this.product.getPrice() * this.count;
+    }
+}
