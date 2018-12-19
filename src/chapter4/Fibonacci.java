d1b7f6a19e2a409e3f0eed2a7d10f9166043680f
sklee <hadesleeq@gmail.com>
2018-12-19 09:35:08 +0900

    Create a tail recursive version of the Fibonacci functional method.

---
+++ src/chapter4/Fibonacci.java
@@ -0,0 +1,9 @@
+package chapter4;
+
+import java.math.BigInteger;
+
+public class Fibonacci  {
+    public static BigInteger fib(int x) {
+        throw new RuntimeException("To be implemented.");
+    }
+}
